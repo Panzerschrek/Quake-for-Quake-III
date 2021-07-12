a0a12617246706dad8a0f9d371c12dfe72f8dfd0
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-07-12 21:33:32 +0300

    Project foundations are created. Map loaders added (source code taken from Quake III BSPC).

---
+++ BspcLib/BspcLibIncludes.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+
+#pragma GCC diagnostic ignored "-Wcomment"
+#endif // __GNUC__
+
+extern "C"
+{
+
+#define false Q_false
+#define true Q_true
+
+#include "l_cmd.h"
+#include "l_math.h"
+#include "l_mem.h"
+#include "l_log.h"
+#include "l_bsp_q1.h"
+#include "l_bsp_q3.h"
+
+#undef true
+#undef false
+
+} // extern "C"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
---
+++ MapConverter/main.cpp
@@ -0,0 +1,5 @@
+#include "BspcLibIncludes.hpp"
+
+int main()
+{
+}
