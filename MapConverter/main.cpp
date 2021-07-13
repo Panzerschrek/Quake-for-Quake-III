3ab971108976b1f7ec5659cbeac4febdbf4bdb6e
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-07-13 21:34:14 +0300

    Start writing exporting code.

--- MapConverter/main.cpp
+++ MapConverter/main.cpp
@@ -1,4 +1,5 @@
 #include "BspcLibIncludes.hpp"
+#include  <cstring>
 
 void AllocateQ3Map()
 {
@@ -36,8 +37,60 @@ void AllocateQ3Map()
 	q3_dfogs = new q3_dfog_t[Q3_MAX_MAP_FOGS];
 }
 
-void ConvertQ1MapIntoQ3Map()
+int ConvertLeaf(const q1_dleaf_t& in_leaf)
 {
+	q3_dleaf_t out_leaf{};
+	std::memcpy(out_leaf.mins, in_leaf.mins, sizeof(float) * 3);
+	std::memcpy(out_leaf.maxs, in_leaf.maxs, sizeof(float) * 3);
+	// TODO - fill other fields.
+
+	const int out_leaf_index = q3_numleafs;
+	q3_dleafs[out_leaf_index]= out_leaf;
+	++q3_numleafs;
+	return out_leaf_index;
+}
+
+// Returns out node/leaf index.
+int ConvertNode(const q1_dnode_t& in_node)
+{
+	q3_dnode_t out_node{};
+	std::memcpy(out_node.mins, in_node.mins, sizeof(float) * 3);
+	std::memcpy(out_node.maxs, in_node.maxs, sizeof(float) * 3);
+
+	for(size_t child_index = 0; child_index < 2; ++child_index)
+	{
+		const short child = in_node.children[child_index];
+		if(child < 0)
+			out_node.children[child_index]= -1 - ConvertLeaf(q1_dleafs[-1 - child]);
+		else
+			out_node.children[child_index]= ConvertNode(q1_dnodes[child]);
+	}
+
+	const int out_node_index = q3_numnodes;
+	q3_dnodes[out_node_index]= out_node;
+	++q3_numnodes;
+	return out_node_index;
+}
+
+void ConvertModel(const q1_dmodel_t& in_model)
+{
+	q3_dmodel_t out_model{};
+	std::memcpy(out_model.mins, in_model.mins, sizeof(float) * 3);
+	std::memcpy(out_model.maxs, in_model.maxs, sizeof(float) * 3);
+	// TODO - fill other fields.
+
+	ConvertNode(q1_dnodes[in_model.headnode[0]]);
+
+	q3_dmodels[q3_nummodels]= out_model;
+	++q3_nummodels;
+}
+
+void ConvertMap()
+{
+	for(int model_index = 0; model_index < q1_nummodels; ++model_index)
+	{
+		ConvertModel(q1_dmodels[model_index]);
+	}
 }
 
 int main()
@@ -45,7 +98,8 @@ int main()
 	Q1_AllocMaxBSP();
 
 	Q1_LoadBSPFile(const_cast<char*>("e1m1.bsp"), 0, 1365176);
+	AllocateQ3Map();
+	ConvertMap();
 	Q1_FreeMaxBSP();
-
 	Q3_WriteBSPFile(const_cast<char*>("q3e1m1.bsp"));
 }
