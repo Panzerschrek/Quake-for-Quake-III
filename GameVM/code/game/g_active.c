39cbdb80218b10a30fcd60df82fb42a7043238e4
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-07-23 00:03:01 +0300

    Remove some server action code for client.

--- GameVM/code/game/g_active.c
+++ GameVM/code/game/g_active.c
@@ -24,265 +24,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "g_local.h"
 
 
-/*
-===============
-G_DamageFeedback
-
-Called just before a snapshot is sent to the given player.
-Totals up all damage and generates both the player_state_t
-damage values to that client for pain blends and kicks, and
-global pain sound events for all clients.
-===============
-*/
-void P_DamageFeedback( gentity_t *player ) {
-	gclient_t	*client;
-	float	count;
-	vec3_t	angles;
-
-	client = player->client;
-	if ( client->ps.pm_type == PM_DEAD ) {
-		return;
-	}
-
-	// total points of damage shot at the player this frame
-	count = client->damage_blood + client->damage_armor;
-	if ( count == 0 ) {
-		return;		// didn't take any damage
-	}
-
-	if ( count > 255 ) {
-		count = 255;
-	}
-
-	// send the information to the client
-
-	// world damage (falling, slime, etc) uses a special code
-	// to make the blend blob centered instead of positional
-	if ( client->damage_fromWorld ) {
-		client->ps.damagePitch = 255;
-		client->ps.damageYaw = 255;
-
-		client->damage_fromWorld = qfalse;
-	} else {
-		vectoangles( client->damage_from, angles );
-		client->ps.damagePitch = angles[PITCH]/360.0 * 256;
-		client->ps.damageYaw = angles[YAW]/360.0 * 256;
-	}
-
-	// play an appropriate pain sound
-	if ( (level.time > player->pain_debounce_time) && !(player->flags & FL_GODMODE) ) {
-		player->pain_debounce_time = level.time + 700;
-		G_AddEvent( player, EV_PAIN, player->health );
-		client->ps.damageEvent++;
-	}
-
-
-	client->ps.damageCount = count;
-
-	//
-	// clear totals
-	//
-	client->damage_blood = 0;
-	client->damage_armor = 0;
-	client->damage_knockback = 0;
-}
-
-
-
-/*
-=============
-P_WorldEffects
-
-Check for lava / slime contents and drowning
-=============
-*/
-void P_WorldEffects( gentity_t *ent ) {
-	qboolean	envirosuit;
-	int			waterlevel;
-
-	if ( ent->client->noclip ) {
-		ent->client->airOutTime = level.time + 12000;	// don't need air
-		return;
-	}
-
-	waterlevel = ent->waterlevel;
-
-	envirosuit = ent->client->ps.powerups[PW_BATTLESUIT] > level.time;
-
-	//
-	// check for drowning
-	//
-	if ( waterlevel == 3 ) {
-		// envirosuit give air
-		if ( envirosuit ) {
-			ent->client->airOutTime = level.time + 10000;
-		}
-
-		// if out of air, start drowning
-		if ( ent->client->airOutTime < level.time) {
-			// drown!
-			ent->client->airOutTime += 1000;
-			if ( ent->health > 0 ) {
-				// take more damage the longer underwater
-				ent->damage += 2;
-				if (ent->damage > 15)
-					ent->damage = 15;
-
-				// don't play a normal pain sound
-				ent->pain_debounce_time = level.time + 200;
-			}
-		}
-	} else {
-		ent->client->airOutTime = level.time + 12000;
-		ent->damage = 2;
-	}
-
-	//
-	// check for sizzle damage (move to pmove?)
-	//
-	if (waterlevel && 
-		(ent->watertype&(CONTENTS_LAVA|CONTENTS_SLIME)) ) {
-		if (ent->health > 0
-			&& ent->pain_debounce_time <= level.time	) {
-
-			if ( envirosuit ) {
-				G_AddEvent( ent, EV_POWERUP_BATTLESUIT, 0 );
-			}
-		}
-	}
-}
-
-
-
-/*
-===============
-G_SetClientSound
-===============
-*/
-void G_SetClientSound( gentity_t *ent ) {
-	if (ent->waterlevel && (ent->watertype&(CONTENTS_LAVA|CONTENTS_SLIME)) ) {
-		ent->client->ps.loopSound = level.snd_fry;
-	} else {
-		ent->client->ps.loopSound = 0;
-	}
-}
-
-
 
 //==============================================================
-
-/*
-==============
-ClientImpacts
-==============
-*/
-void ClientImpacts( gentity_t *ent, pmove_t *pm ) {
-	int		i, j;
-	trace_t	trace;
-	gentity_t	*other;
-
-	memset( &trace, 0, sizeof( trace ) );
-	for (i=0 ; i<pm->numtouch ; i++) {
-		for (j=0 ; j<i ; j++) {
-			if (pm->touchents[j] == pm->touchents[i] ) {
-				break;
-			}
-		}
-		if (j != i) {
-			continue;	// duplicated
-		}
-		other = &g_entities[ pm->touchents[i] ];
-
-		if ( ( ent->r.svFlags & SVF_BOT ) && ( ent->touch ) ) {
-			ent->touch( ent, other, &trace );
-		}
-
-		if ( !other->touch ) {
-			continue;
-		}
-
-		other->touch( other, ent, &trace );
-	}
-
-}
-
-/*
-============
-G_TouchTriggers
-
-Find all trigger entities that ent's current position touches.
-Spectators will only interact with teleporters.
-============
-*/
-void	G_TouchTriggers( gentity_t *ent ) {
-	int			i, num;
-	int			touch[MAX_GENTITIES];
-	gentity_t	*hit;
-	trace_t		trace;
-	vec3_t		mins, maxs;
-	static vec3_t	range = { 40, 40, 52 };
-
-	if ( !ent->client ) {
-		return;
-	}
-
-	// dead clients don't activate triggers!
-	if ( ent->client->ps.stats[STAT_HEALTH] <= 0 ) {
-		return;
-	}
-
-	VectorSubtract( ent->client->ps.origin, range, mins );
-	VectorAdd( ent->client->ps.origin, range, maxs );
-
-	num = trap_EntitiesInBox( mins, maxs, touch, MAX_GENTITIES );
-
-	// can't use ent->absmin, because that has a one unit pad
-	VectorAdd( ent->client->ps.origin, ent->r.mins, mins );
-	VectorAdd( ent->client->ps.origin, ent->r.maxs, maxs );
-
-	for ( i=0 ; i<num ; i++ ) {
-		hit = &g_entities[touch[i]];
-
-		if ( !hit->touch && !ent->touch ) {
-			continue;
-		}
-		if ( !( hit->r.contents & CONTENTS_TRIGGER ) ) {
-			continue;
-		}
-
-		// ignore most entities if a spectator
-		if ( ent->client->sess.sessionTeam == TEAM_SPECTATOR ) {
-			if ( hit->s.eType != ET_TELEPORT_TRIGGER ) {
-				continue;
-			}
-		}
-
-		// use separate code for determining if an item is picked up
-		// so you don't have to actually contact its bounding box
-		if ( hit->s.eType == ET_ITEM ) {
-		} else {
-			if ( !trap_EntityContact( mins, maxs, hit ) ) {
-				continue;
-			}
-		}
-
-		memset( &trace, 0, sizeof(trace) );
-
-		if ( hit->touch ) {
-			hit->touch (hit, ent, &trace);
-		}
-
-		if ( ( ent->r.svFlags & SVF_BOT ) && ( ent->touch ) ) {
-			ent->touch( ent, hit, &trace );
-		}
-	}
-
-	// if we didn't touch a jump pad this pmove frame
-	if ( ent->client->ps.jumppad_frame != ent->client->ps.pmove_framecount ) {
-		ent->client->ps.jumppad_frame = 0;
-		ent->client->ps.jumppad_ent = 0;
-	}
-}
 
 /*
 =================
@@ -321,176 +64,12 @@ void SpectatorThink( gentity_t *ent, usercmd_t *ucmd ) {
 		// save results of pmove
 		VectorCopy( client->ps.origin, ent->s.origin );
 
-		G_TouchTriggers( ent );
 		trap_UnlinkEntity( ent );
 	}
 
 	client->oldbuttons = client->buttons;
 	client->buttons = ucmd->buttons;
 }
-
-
-
-/*
-=================
-ClientInactivityTimer
-
-Returns qfalse if the client is dropped
-=================
-*/
-qboolean ClientInactivityTimer( gclient_t *client ) {
-	if ( ! g_inactivity.integer ) {
-		// give everyone some time, so if the operator sets g_inactivity during
-		// gameplay, everyone isn't kicked
-		client->inactivityTime = level.time + 60 * 1000;
-		client->inactivityWarning = qfalse;
-	} else if ( client->pers.cmd.forwardmove || 
-		client->pers.cmd.rightmove || 
-		client->pers.cmd.upmove ||
-		(client->pers.cmd.buttons & BUTTON_ATTACK) ) {
-		client->inactivityTime = level.time + g_inactivity.integer * 1000;
-		client->inactivityWarning = qfalse;
-	} else if ( !client->pers.localClient ) {
-		if ( level.time > client->inactivityTime ) {
-			trap_DropClient( client - level.clients, "Dropped due to inactivity" );
-			return qfalse;
-		}
-		if ( level.time > client->inactivityTime - 10000 && !client->inactivityWarning ) {
-			client->inactivityWarning = qtrue;
-			trap_SendServerCommand( client - level.clients, "cp \"Ten seconds until inactivity drop!\n\"" );
-		}
-	}
-	return qtrue;
-}
-
-/*
-==================
-ClientTimerActions
-
-Actions that happen once a second
-==================
-*/
-void ClientTimerActions( gentity_t *ent, int msec ) {
-	gclient_t	*client;
-
-	client = ent->client;
-	client->timeResidual += msec;
-
-	while ( client->timeResidual >= 1000 ) {
-		client->timeResidual -= 1000;
-
-		// regenerate
-		if ( client->ps.powerups[PW_REGEN] ) {
-			if ( ent->health < client->ps.stats[STAT_MAX_HEALTH]) {
-				ent->health += 15;
-				if ( ent->health > client->ps.stats[STAT_MAX_HEALTH] * 1.1 ) {
-					ent->health = client->ps.stats[STAT_MAX_HEALTH] * 1.1;
-				}
-				G_AddEvent( ent, EV_POWERUP_REGEN, 0 );
-			} else if ( ent->health < client->ps.stats[STAT_MAX_HEALTH] * 2) {
-				ent->health += 5;
-				if ( ent->health > client->ps.stats[STAT_MAX_HEALTH] * 2 ) {
-					ent->health = client->ps.stats[STAT_MAX_HEALTH] * 2;
-				}
-				G_AddEvent( ent, EV_POWERUP_REGEN, 0 );
-			}
-		} else {
-			// count down health when over max
-			if ( ent->health > client->ps.stats[STAT_MAX_HEALTH] ) {
-				ent->health--;
-			}
-		}
-
-		// count down armor when over max
-		if ( client->ps.stats[STAT_ARMOR] > client->ps.stats[STAT_MAX_HEALTH] ) {
-			client->ps.stats[STAT_ARMOR]--;
-		}
-	}
-}
-
-/*
-====================
-ClientIntermissionThink
-====================
-*/
-void ClientIntermissionThink( gclient_t *client ) {
-	client->ps.eFlags &= ~EF_TALK;
-	client->ps.eFlags &= ~EF_FIRING;
-
-	// the level will exit when everyone wants to or after timeouts
-
-	// swap and latch button actions
-	client->oldbuttons = client->buttons;
-	client->buttons = client->pers.cmd.buttons;
-	if ( client->buttons & ( BUTTON_ATTACK | BUTTON_USE_HOLDABLE ) & ( client->oldbuttons ^ client->buttons ) ) {
-		// this used to be an ^1 but once a player says ready, it should stick
-		client->readyToExit = 1;
-	}
-}
-
-
-/*
-================
-ClientEvents
-
-Events will be passed on to the clients for presentation,
-but any server game effects are handled here
-================
-*/
-void ClientEvents( gentity_t *ent, int oldEventSequence ) {
-	int		i, j;
-	int		event;
-	gclient_t *client;
-	int		damage;
-	vec3_t	origin, angles;
-//	qboolean	fired;
-	gitem_t *item;
-	gentity_t *drop;
-
-	client = ent->client;
-
-	if ( oldEventSequence < client->ps.eventSequence - MAX_PS_EVENTS ) {
-		oldEventSequence = client->ps.eventSequence - MAX_PS_EVENTS;
-	}
-	for ( i = oldEventSequence ; i < client->ps.eventSequence ; i++ ) {
-		event = client->ps.events[ i & (MAX_PS_EVENTS-1) ];
-
-		switch ( event ) {
-		case EV_FALL_MEDIUM:
-		case EV_FALL_FAR:
-			if ( ent->s.eType != ET_PLAYER ) {
-				break;		// not in the player model
-			}
-			if ( g_dmflags.integer & DF_NO_FALLING ) {
-				break;
-			}
-			if ( event == EV_FALL_FAR ) {
-				damage = 10;
-			} else {
-				damage = 5;
-			}
-			ent->pain_debounce_time = level.time + 200;	// no normal pain sound
-			break;
-
-		case EV_FIRE_WEAPON:
-			break;
-
-		case EV_USE_ITEM1:		// teleporter
-			break;
-
-		case EV_USE_ITEM2:		// medkit
-			ent->health = ent->client->ps.stats[STAT_MAX_HEALTH] + 25;
-
-			break;
-
-		default:
-			break;
-		}
-	}
-
-}
-
-void BotTestSolid(vec3_t origin);
 
 /*
 ==============
@@ -588,25 +167,12 @@ void ClientThink_real( gentity_t *ent ) {
 		//	return;
 	}
 
-	//
-	// check for exiting intermission
-	//
-	if ( level.intermissiontime ) {
-		ClientIntermissionThink( client );
-		return;
-	}
-
 	// spectators don't do much
 	if ( client->sess.sessionTeam == TEAM_SPECTATOR ) {
 		if ( client->sess.spectatorState == SPECTATOR_SCOREBOARD ) {
 			return;
 		}
 		SpectatorThink( ent, ucmd );
-		return;
-	}
-
-	// check for inactivity timer, but never drop the local client of a non-dedicated server
-	if ( !ClientInactivityTimer( client ) ) {
 		return;
 	}
 
@@ -682,23 +248,12 @@ void ClientThink_real( gentity_t *ent ) {
 	VectorCopy (pm.mins, ent->r.mins);
 	VectorCopy (pm.maxs, ent->r.maxs);
 
-	ent->waterlevel = pm.waterlevel;
-	ent->watertype = pm.watertype;
-
-	// execute client events
-	ClientEvents( ent, oldEventSequence );
-
 	// link entity now, after any personal teleporters have been used
 	trap_LinkEntity (ent);
-	if ( !ent->client->noclip ) {
-		G_TouchTriggers( ent );
-	}
 
 	// NOTE: now copy the exact origin over otherwise clients can be snapped into solid
 	VectorCopy( ent->client->ps.origin, ent->r.currentOrigin );
 
-	// touch other objects
-	ClientImpacts( ent, &pm );
 
 	// save results of triggers and client events
 	if (ent->client->ps.eventSequence != oldEventSequence) {
@@ -728,9 +283,6 @@ void ClientThink_real( gentity_t *ent ) {
 		}
 		return;
 	}
-
-	// perform once-a-second actions
-	ClientTimerActions( ent, msec );
 }
 
 /*
@@ -854,11 +406,6 @@ void ClientEndFrame( gentity_t *ent ) {
 		return;
 	}
 
-	// burn from lava, etc
-	P_WorldEffects (ent);
-
-	// apply all the damage taken this frame
-	P_DamageFeedback (ent);
 
 	// add the EF_CONNECTION flag if we haven't gotten commands recently
 	if ( level.time - ent->client->lastCmdTime > 1000 ) {
@@ -868,8 +415,6 @@ void ClientEndFrame( gentity_t *ent ) {
 	}
 
 	ent->client->ps.stats[STAT_HEALTH] = ent->health;	// FIXME: get rid of ent->health...
-
-	G_SetClientSound (ent);
 
 	// set the latest infor
 	SendPendingPredictableEvents( &ent->client->ps );
