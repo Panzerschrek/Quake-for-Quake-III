2d9a0c006b0c1679a46c05074ddc1127ad881781
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-08-17 22:37:00 +0300

    Simlify formatting.

--- extract_and_convert_textures.py
+++ extract_and_convert_textures.py
@@ -40,7 +40,8 @@ shader_with_fullbrights_template = """
 		blendFunc GL_ONE GL_ONE
 		rgbGen identity
 	}
-}"""
+}
+"""
 
 shader_water_template = """
 "textures/%(shader_name)"
@@ -59,7 +60,8 @@ shader_water_template = """
 		blendFunc GL_ONE GL_ZERO
 		rgbGen identity
 	}
-}"""
+}
+"""
 
 shader_slime_template = """
 "textures/%(shader_name)"
@@ -78,7 +80,8 @@ shader_slime_template = """
 		blendFunc GL_ONE GL_ZERO
 		rgbGen identity
 	}
-}"""
+}
+"""
 
 shader_lava_template = """
 "textures/%(shader_name)"
@@ -98,7 +101,8 @@ shader_lava_template = """
 		blendFunc GL_ONE GL_ZERO
 		rgbGen identity
 	}
-}"""
+}
+"""
 
 shader_generic_turb_template = """
 "textures/%(shader_name)"
@@ -116,7 +120,8 @@ shader_generic_turb_template = """
 		blendFunc GL_ONE GL_ZERO
 		rgbGen identity
 	}
-}"""
+}
+"""
 
 shader_animated_template = """
 "textures/%(shader_name)"
@@ -160,20 +165,20 @@ def generate_shader_file(tga_textures_dir, out_shader_file):
 			file_name_without_extension = file_name.replace(".tga", "")
 			fullbrights_file_name = file_name_without_extension + "_fb.tga"
 
-			shader_descr = ""
+			shader = ""
 			if file_name.endswith("_fb.tga"):
 				continue # ignore fullbright textures
 			elif file_name.startswith("SKY"): # ignore skies for now
 				continue
 			elif file_name.startswith("*"):
 				if file_name.find("WATER") != -1:
-					shader_descr = shader_water_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
+					shader = shader_water_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
 				elif file_name.find("SLIME") != -1:
-					shader_descr = shader_slime_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
+					shader = shader_slime_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
 				elif file_name.find("LAVA") != -1:
-					shader_descr = shader_lava_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
+					shader = shader_lava_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
 				else:
-					shader_descr = shader_generic_turb_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
+					shader = shader_generic_turb_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name)
 			elif file_name.startswith("+"):
 				# animated texture
 
@@ -206,23 +211,21 @@ def generate_shader_file(tga_textures_dir, out_shader_file):
 						fullbright_files_list = []
 						for frame in fulbright_frames_list:
 							fullbright_files_list.append("\"textures/" + frame + "\"")
-						shader_descr = shader_animated_fullbright_template.replace("%(shader_name)", file_name_without_extension).replace("%(files_list)", " ".join(files_list)).replace("%(fullbright_files_list)", " ".join(fullbright_files_list))
+						shader = shader_animated_fullbright_template.replace("%(shader_name)", file_name_without_extension).replace("%(files_list)", " ".join(files_list)).replace("%(fullbright_files_list)", " ".join(fullbright_files_list))
 					else:
 						# Generate regular animation
 						files_list = []
 						for frame in frames_list:
 							files_list.append("\"textures/" + frame + "\"")
-						shader_descr = shader_animated_template.replace("%(shader_name)", file_name_without_extension).replace("%(files_list)", " ".join(files_list))
-					f.write(shader_descr + "\n")
-					continue
+						shader = shader_animated_template.replace("%(shader_name)", file_name_without_extension).replace("%(files_list)", " ".join(files_list))
 
 			# Not a sky, turb or animation - try to create shader with only fullbright component.
-			if shader_descr == "" and os.path.exists(os.path.join(tga_textures_dir, fullbrights_file_name)):
-				shader_descr = shader_with_fullbrights_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name).replace("%(fullbrights_file_name)", fullbrights_file_name)
+			if shader == "" and os.path.exists(os.path.join(tga_textures_dir, fullbrights_file_name)):
+				shader = shader_with_fullbrights_template.replace("%(shader_name)", file_name_without_extension).replace("%(file_name)", file_name).replace("%(fullbrights_file_name)", fullbrights_file_name)
 
 			# Do not create shader for regular textures without turb, animation, fullbright pixels.
-			if shader_descr != "":
-				f.write(shader_descr + "\n")
+			if shader != "":
+				f.write(shader)
 
 
 def main():
