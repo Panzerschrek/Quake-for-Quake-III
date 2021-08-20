d0025a82f9b46f65925509bcfa34f1a45e67bdd2
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-08-20 21:21:00 +0300

    Start writing conversion code. Export some surfaces.

--- qwalk/model_q1bsp.c
+++ qwalk/model_q1bsp.c
@@ -59,12 +59,118 @@ static bool_t load_q1bsp_moddel(void *filedata, size_t filesize, char **out_erro
 	return true;
 }
 
+
+static void convert_q1bsp(model_t *out_model)
+{
+	mem_pool_t *pool;
+	model_t model;
+	int	i, j, k;
+	const int	max_vertices = 1024;
+
+	pool = mem_create_pool();
+
+	memset(&model, 0, sizeof(model));
+
+	model.num_frames = 1;
+	model.frameinfo = (frameinfo_t*)mem_alloc(pool, sizeof(frameinfo_t));
+	memset(model.frameinfo, 0, sizeof(frameinfo_t));
+	model.frameinfo->num_frames = 1;
+	model.frameinfo->frametime = 1.0f;
+
+	model.frameinfo->frames = (singleframe_t*)mem_alloc(pool, sizeof(singleframe_t));
+	memset(model.frameinfo->frames, 0, sizeof(singleframe_t));
+	model.frameinfo->frames->offset = 0;
+	model.frameinfo->frames->name = mem_copystring(pool, "some frame");
+
+	model.num_meshes = q1_numtexinfo;
+	model.meshes = (mesh_t*)mem_alloc(pool, model.num_meshes * sizeof(mesh_t));
+	memset(model.meshes, 0, model.num_meshes * sizeof(mesh_t));
+
+	for (i= 0; i < model.num_meshes; ++i)
+	{
+		model.meshes[i].name = mem_copystring(pool, "some mesh");
+		model.meshes[i].vertex3f = mem_alloc(pool, max_vertices * sizeof(float) * 3);
+		model.meshes[i].normal3f = mem_alloc(pool, max_vertices * sizeof(float) * 3);
+		model.meshes[i].texcoord2f = mem_alloc(pool, max_vertices * sizeof(float) * 2);
+		model.meshes[i].triangle3i = mem_alloc(pool, max_vertices * sizeof(int) * 3);
+	}
+
+	for (i = 0; i < q1_numfaces; ++i)
+	{
+		q1_dface_t* face;
+		float normal_sign, normal[3], tex_scale[2];
+		int edge_index, first_vertex;
+		mesh_t* mesh;
+		q1_texinfo_t* tex;
+
+		face = q1_dfaces + i;
+
+		normal_sign= face->side == 0 ? 1.0f : -1.0f;
+		for(j= 0; j < 3; ++j)
+			normal[j]= q1_dplanes[face->planenum].normal[j] * normal_sign;
+
+		tex= &q1_texinfo[face->texinfo];
+
+		tex_scale[0]= tex_scale[1]= 1.0f; // TODO - set this
+
+		mesh = &model.meshes[0]; // TODO - use different meshes
+
+		// Extract vertices.
+		first_vertex = mesh->num_vertices;
+		for(edge_index= 0; edge_index < face->numedges; ++edge_index)
+		{
+			int e, vert_num;
+			q1_dvertex_t* in_vertex;
+
+			e= q1_dsurfedges[face->firstedge + edge_index];
+			in_vertex =
+				e >= 0
+					? &q1_dvertexes[ q1_dedges[+e].v[0] ]
+					: &q1_dvertexes[ q1_dedges[-e].v[1] ];
+
+			vert_num = first_vertex + edge_index;
+			memcpy(mesh->vertex3f + vert_num * 3, in_vertex->point, sizeof(float) * 3);
+			memcpy(mesh->normal3f + vert_num * 3, in_vertex->point, sizeof(float) * 3);
+			memcpy(mesh->texcoord2f + vert_num * 2, normal, sizeof(float) * 2);
+
+			for(int k= 0; k < 2; ++k)
+			{
+				mesh->texcoord2f[vert_num * 2 + k]=
+					tex->vecs[k][0] * in_vertex->point[0] +
+					tex->vecs[k][1] * in_vertex->point[1] +
+					tex->vecs[k][2] * in_vertex->point[2] +
+					tex->vecs[k][3];
+				mesh->texcoord2f[vert_num * 2 + k]*= tex_scale[i];
+			}
+		}
+
+		// Create triangles.
+		int first_triangle = mesh->num_triangles;
+		for(edge_index= 0; edge_index < face->numedges - 2; ++edge_index)
+		{
+			int triangle_num;
+			triangle_num = first_triangle + edge_index;
+			mesh->triangle3i[triangle_num * 3 + 0]= first_vertex;
+			mesh->triangle3i[triangle_num * 3 + 1]= first_vertex + edge_index + 1;
+			mesh->triangle3i[triangle_num * 3 + 2]= first_vertex + edge_index + 2;
+		}
+
+		mesh->num_vertices+= face->numedges;
+		mesh->num_triangles+= face->numedges - 2;
+	}
+
+	*out_model= model;
+}
+
 bool_t model_q1bsp_load(void *filedata, size_t filesize, model_t *out_model, char **out_error)
 {
+
 	Q1_AllocMaxBSP();
 
 	if (!load_q1bsp_moddel(filedata, filesize, out_error))
 		return false;
+
+	convert_q1bsp(out_model);
 
 	return true;
 }
